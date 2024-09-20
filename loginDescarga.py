f6e7438c41d3a46e09fef1a259f87ea3d94fab79
Jonathan Marquez <jonicv_6@hotmail.com>
2024-09-20 18:36:03 +0200

    Configuracion Chrome

--- loginDescarga.py
+++ loginDescarga.py
@@ -9,11 +9,15 @@ from selenium.webdriver.chrome.options import Options
 
 # Configurar las opciones de Chrome
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--headless')  # Opcional para ejecución sin interfaz gráfica
+chrome_options.add_argument('--no-sandbox')  # Importante para evitar problemas de permisos en entornos Docker
+chrome_options.add_argument('--disable-dev-shm-usage')  # Usa /tmp en lugar de /dev/shm
+chrome_options.add_argument('--headless')  # Ejecutar sin interfaz gráfica
+chrome_options.add_argument('--disable-gpu')  # Desactivar GPU, ya que no se necesita en contenedores
+chrome_options.add_argument('--disable-software-rasterizer')  # Evita problemas con la aceleración de gráficos
+chrome_options.add_argument('--disable-extensions')  # Desactivar las extensiones de Chrome
+chrome_options.add_argument('--remote-debugging-port=9222')  # Habilitar depuración remota
 
-# Selenium automáticamente descargará la versión correcta de ChromeDriver
+# Inicializar el controlador de Chrome
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 # Navega a la web que deseas automatizar
